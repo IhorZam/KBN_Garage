869b7a4c7f16ae973f5c59f52ccba5ff698b580d
IhorZam <ihorzam25@gmail.com>
2018-04-26 17:09:38 +0300

    Added tooltips

--- Frontend/JS/Main_Page.js
+++ Frontend/JS/Main_Page.js
@@ -1,3 +1,35 @@
 $(function(){
 
+    var data = require('./data/data');
+
+    localStorage.setItem("isLogged", "false");
+
+    // Function to check if the user is logged in
+    var is_logged = function () {
+      return localStorage.getItem("isLogged") === 'true'? true : false;
+    };
+
+
+    // Central element
+    $center = $('.center');
+    var buttons = {
+        engine : [$($center.find('#engine')), "engine"],
+        odo : [$($center.find('#odo')), "odo"],
+        srs : [$($center.find('#srs')), "srs"],
+        immo : [$($center.find('#immo')), "immo"],
+        abs : [$($center.find('#abs')), "abs"],
+        ecu : [$($center.find('#ecu')), "ecu"],
+        ps : [$($center.find('#ps')), "ps"],
+        dpf : [$($center.find('#dpf')), "dpf"]
+    };
+
+    function addAttrs(element){
+        element[0].attr("data-tooltip",data[element[1]].tooltip);
+        if (is_logged()) element[0].attr("href", "request.html");
+        else element[0].attr("href", "form.html");
+    }
+
+    buttons.forEach(addAttrs);
+
+
 });
--- Frontend/JS/data/data.js
+++ Frontend/JS/data/data.js
@@ -1,67 +1,3 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-$(function(){
-
-    var data = require('./data/data');
-
-    localStorage.setItem("isLogged", "false");
-
-    // Function to check if the user is logged in
-    var is_logged = function () {
-      return localStorage.getItem("isLogged") === 'true'? true : false;
-    };
-
-
-    // Central element
-    $center = $('.center');
-    $engine_button = $($center.find('#engine'));
-    $odo_button = $($center.find('#odo'));
-    $srs_button = $($center.find('#srs'));
-    $immo_button = $($center.find('#immo'));
-    $abs_button = $($center.find('#abs'));
-    $ecu_button = $($center.find('#ecu'));
-    $ps_button = $($center.find('#ps'));
-    $dpf_button = $($center.find('#dpf'));
-
-    $engine_button.attr("data-tooltip",data['engine'].tooltip);
-    if (is_logged()) $engine_button.attr("href", "request.html");
-    else $engine_button.attr("href", "form.html");
-
-    $odo_button.attr("data-tooltip",data['odo'].tooltip);
-    if (is_logged()) $odo_button.attr("href", "request.html");
-    else $odo_button.attr("href", "form.html");
-
-    $srs_button.attr("data-tooltip",data['srs'].tooltip);
-    if (is_logged()) $srs_button.attr("href", "request.html");
-    else $srs_button.attr("href", "form.html");
-
-    $immo_button.attr("data-tooltip",data['immo'].tooltip);
-    if (is_logged()) $immo_button.attr("href", "request.html");
-    else $immo_button.attr("href", "form.html");
-
-    $abs_button.attr("data-tooltip",data['abs'].tooltip);
-    if (is_logged()) $abs_button.attr("href", "request.html");
-    else $abs_button.attr("href", "form.html");
-
-    $ecu_button.attr("data-tooltip",data['ecu'].tooltip);
-    if (is_logged()) $ecu_button.attr("href", "request.html");
-    else $ecu_button.attr("href", "form.html");
-
-    $ps_button.attr("data-tooltip",data['ps'].tooltip);
-    if (is_logged()) $ps_button.attr("href", "request.html");
-    else $ps_button.attr("href", "form.html");
-
-    $dpf_button.attr("data-tooltip",data['dpf'].tooltip);
-    if (is_logged()) $dpf_button.attr("href", "request.html");
-    else $dpf_button.attr("href", "form.html");
-
-
-
-
-
-
-
-});
-},{"./data/data":2}],2:[function(require,module,exports){
 
 
 var data = {
@@ -156,4 +92,3 @@ var data = {
 
 
 module.exports = data;
-},{}]},{},[1]);
