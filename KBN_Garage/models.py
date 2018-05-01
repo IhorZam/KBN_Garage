b355c1a17ac87d4e0339a1b49d4c4f38a19f08b5
Stas Telizhenko <alex@alexcp.com>
2018-05-01 17:30:48 +0300

    api for authorisation

---
+++ KBN_Garage/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.db import models
+import random
+import string
+
+
+class SessionManager(models.Manager):
+    @staticmethod
+    def authentificate(username, password):
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                session = Session()
+                session.user = user
+                session.key = "".join(random.choice(string.ascii_letters) for _ in range(64))
+                session.save()
+                return session.key
+        return None
+
+    @staticmethod
+    def exit(session):
+        session.delete()
+
+
+class Session(models.Model):
+    key = models.CharField(unique=True, max_length=64)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = SessionManager()
+
+
+def add_user(username, password, email, phone):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    user.save()
+    disp = Phone.objects.create(num=phone, user=user)
+    disp.save()
+
+
+class Phone(models.Model):
+    num = models.CharField(max_length=16)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
--- KBN_Garage/views.py
+++ KBN_Garage/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, HttpResponsePermanentRedirect, HttpResponseRedirect, render_to_response
+from django.shortcuts import render_to_response
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from KBN_Garage.models import *
 
 
-def main(request):
+def main(req):
     return render_to_response('index.html')
 
 
@@ -11,3 +15,46 @@ def request(req):
 
 def form(req):
     return render_to_response('form.html')
+
+
+# API
+@csrf_exempt
+def sign_up(req):
+    if req.method == 'POST':
+        data = json.loads(req.body.decode('utf-8'))
+        username = data['username']
+        password = data['password']
+        email = data['email']
+        phone = data['telephone']
+        if not User.objects.filter(username=username).exists():
+            add_user(username, password, email, phone)
+            key = Session.objects.authentificate(username, password)
+            response = JsonResponse({'status': 'ok'})
+            response.set_cookie('sessid', key)
+            return response
+        return JsonResponse({'status': 'error'})
+
+
+@csrf_exempt
+def sign_in(req):
+    if request.method == 'POST':
+        data = json.loads(req.body.decode('utf-8'))
+        username = data['username']
+        password = data['password']
+        sessid = Session.objects.authentificate(username, password)
+        if sessid:
+            response = JsonResponse({'status': 'ok'})
+            response.set_cookie('sessid', sessid)
+            return response
+    return JsonResponse({'status': 'error'})
+
+
+@csrf_exempt
+def sign_out(req):
+    Session.objects.exit(req.session)
+    return JsonResponse({'status': 'ok'})
+
+
+@csrf_exempt
+def authorised(req):
+    return JsonResponse({'auth': req.user is not None})
